17a67f001066ed83fc5eea0eaf50957b906d68b7
Gabriel Castro <gacagonz@espol.edu.ec>
2020-12-02 20:33:51 -0500

    Inicialización de clase sistema

--- src/main/java/pantallas/Sistema.java
+++ src/main/java/pantallas/Sistema.java
@@ -5,22 +5,49 @@
  */
 package pantallas;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import tdas_linkedlist.CircularLinkedList;
 
 /**
  *
- * @author david
+ * @author david, Gabriel
  */
 public class Sistema {
     
-    HBox contenido = new HBox();
+    Pane contenido;
+    Label titulo;
+    CircularLinkedList soldados;
+    Label cantidadSelec;
+    Slider posicionInicio;
+    Pane imagenesSoldado;
+    HBox opciones;
+    VBox direccion;
+    Label direccionSelec;
+    Button izquierda;
+    Button derecha;
+    ImageView fondo;
+    Button simular;
     
     public Sistema() {
     }
     
-    public HBox getContenido() {
-        return contenido;
+    public Pane getContenido() {
+        return this.contenido;
     }
-
+    
+    public void agregarContenido() {
+        
+    }
+    
+    public void iniciarContenido() {
+        
+    }
+    
 }
